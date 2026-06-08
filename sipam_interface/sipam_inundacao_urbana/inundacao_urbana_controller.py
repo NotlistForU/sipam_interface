64969a92ceb0267bdc2e68a88822c8380c28c5a9
GabrieOliveiraMartins <gabrolivmartins@gmail.com>
2026-06-08 12:29:19 -0400

    Primeiro commit

---
+++ sipam_interface/sipam_inundacao_urbana/inundacao_urbana_controller.py
@@ -0,0 +1,28 @@
+
+# lisagem_dados():
+#   if request.method == 'GET:
+#       registros = service.listagem_dados()
+#
+
+
+# cadastro_dados():
+#   if request.method == 'POST':
+#       dados_reqisicao = json.loads(request.body)
+#       novo_registro = services.cadastro_dados(dados_requisicao);
+#       return JsonResponse({'mensagem': 'Cadastrado', 'id': novo_registro.id}, status=201)
+#   
+
+
+
+# edicao_dados():
+#   registro = InundacaoUrbana.objects.get(id=1);
+#   registro.municipio = "Novo Valor";
+#   registro.save();
+#
+
+# exclusao():
+#   registro = InundacaoUrbana.objects.get(id=1);
+#   registro.status = False;
+#
+
+
---
+++ sipam_interface/sipam_inundacao_urbana/model/Entity.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+class InundacaoUrbana(models.Model):
+    id = models.IntegerField(primary_key=True);
+    cd_geocodi = models.CharField(max_length=15, null=True, blank=True);
+    municipio = models.CharField(max_lenght=80, null=True, blank=True);
+    cd_estacao = models.IntegerField(null=True, blank=True);
+    longitude = models.DecimalField(
+        max_digits=12,
+        decimal_places=9,
+        null=True,
+        blank=True
+    );
+
+    latitude = models.DecimalField(
+        max_digits=12,
+        decimal_places=8,
+        null=True,
+        blank=True
+    )
+
+    cota_rn_ana = models.IntegerField(null=True, blank=True);
+    cota_rn_orto = models.IntegerField(null=True, blank=True);
+
+    cota_atencao = models.FloatField(null=True, blank=True);
+    cota_alerta = models.FloatField(null=True, blank=True);
+    cota_inundacao = models.FloatField(null=True, blank=True);
+
+    data_voo = models.DateField(null=True, blank=True);
+
+    satus = models.BooleanField(null=True, blank=True);
+    integrado = models.BooleanField(null=True, blank=True);
+
+    class Meta:
+        db_table = 'sipam_inundacao_urbana'
+        managed = False ######
+
+    def __str__(self):
+        return self.municipio or f'Inundação Urbana {self.id}';
